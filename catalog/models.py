3065da1e42193351220161679a7c5396671d55d5
Konstantin Safarov <safarov-konstantin@yandex.ru>
2023-12-21 03:16:07 +0300

    Задание 3 Описать модели Product Category. Настроить админ панель и медиа
    	изменено:      catalog/models.py
    	изменено:      config/settings.py
    	изменено:      config/urls.py
    	изменено:      requirements.txt
    
    Задание 4 Перенеси отображение моделей в базу данных с помощью инструмента миграций
    	выполнены пункты:
    		1 Создание миграций для новый моделей
    		2 Применение миграций
    
    	новый файл:    catalog/migrations/0001_initial.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,35 @@
 from django.db import models
 
 
+NULLABLE = {'null': True, 'blank': True}
+
+
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=3, decimal_places=0, verbose_name='цена')
+    date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    date_of_last_changing = models.DateTimeField(verbose_name='дата последнего изменения', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.pk} {self.name}'
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ('name',)
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self) -> str:
+        return f'{self.pk} {self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('name', )
