7ed7859d5e8f5535075d75c167d1ad05f4b1e135
Konstantin Safarov <safarov-konstantin@yandex.ru>
2024-01-16 23:29:55 +0300

    Исправления
    Так как ты написал вьюшку ProductDetailView, то catalog_product больне не нужна.
    Так же нужно удалить остальной неиспользуемый или закомментированный код в проекте.
    Вьюшку  catalog_contacts нужно тоже перевести в CBV (тут подойдет TemplateView)
    
    У модели блока поле views_count не может быть меньше нуля,
    это можно отразить в модели указав PositiveIntegerField

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,21 +1,14 @@
 from django.urls import path
 # from catalog.views import catalog_index, catalog_contacts, catalog_product
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView, catalog_contacts
+from catalog.views import ProductListView, ProductDetailView, ContatactTemplateView
 
 
 app_name = CatalogConfig.name
 
-# urls cbv
+
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
-    path('contacts/', catalog_contacts, name='contacts'),
+    path('contacts/', ContatactTemplateView.as_view(), name='contacts'),
     path('<int:pk>/product/', ProductDetailView.as_view(), name='product'),
 ]
-
-# urls fbv
-# urlpatterns = [
-#     path('', catalog_index, name='index'),
-#     path('contacts/', catalog_contacts, name='contacts'),
-#     path('<int:pk>/product/', catalog_product, name='product'),
-# ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Product
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
 # cbv
 class ProductListView(ListView):
@@ -11,20 +11,5 @@ class ProductDetailView(DetailView):
     model = Product
 
 
-# fbv 
-def catalog_contacts(request):
-    return render(request, 'catalog/contacts.html')
-
-
-# def catalog_index(request):
-#     context = {
-#         'object_list': Product.objects.all()
-#     }
-#     return render(request, 'catalog/index.html', context=context)
-
-
-def catalog_product(request, pk):
-    context = {
-        'object': Product.objects.get(pk=pk)
-    }
-    return render(request, 'catalog/product.html', context=context)
+class ContatactTemplateView(TemplateView):
+    template_name = 'catalog/contacts.html/'
